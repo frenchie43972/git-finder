ee4a63c8e56ea380a48dabc4c43a2f8de6c3c272
Kris French <frenchie43972@gmail.com>
2023-02-13 17:47:25 +0900

    Initial set up with some extras

---
+++ git-finder/src/pages/About.jsx
@@ -0,0 +1,19 @@
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl text-white mb-4">GitHub Finder</h1>
+      <p className='mb-4 text-2xl text-white font-light'>
+        A React app to search GitHub profiles and view profile details. This project is part of the <i>React Front To Back</i> Udemy course by Brad Traversy.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-black-200'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By: Hassib Moddasser
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ git-finder/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-6xl text-gray-300">
+        Hello, Everything is Fine!
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
