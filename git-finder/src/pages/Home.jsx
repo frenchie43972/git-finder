185b70f80f8cc44e2dac69a18566ce8491dfece3
French <1078924850117002@mil>
2023-02-14 14:16:37 +0900

    small update

--- git-finder/src/pages/Home.jsx
+++ git-finder/src/pages/Home.jsx
@@ -1,11 +1,10 @@
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl text-gray-300">
-        Hello, Everything is Fine!
-      </h1>
-    </div>
+    <>
+      <UserResults />
+    </>
   );
 }
 
