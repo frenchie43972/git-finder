210f757c00b6f391082094199d079f0d09d01fc2
French <1078924850117002@mil>
2023-02-16 14:46:39 +0900

    building the search function

--- git-finder/src/context/github/GithubContext.js
+++ git-finder/src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,26 +7,43 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: false,
+  };
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const searchUsers = async () => {
+    setLoading();
+
+    // URLSearchParams is a built in browser API that provides a way to work with 
+    // the query parameters (the key/value pairs after the ? in a URL)
+    const params = new URLSearchParams({
+
+    });
+    
+    const response = await fetch(`${GITHUB_URL}/search/users/${}`, {
       headers: {
           Authorization: `Bearer ${GITHUB_TOKEN}`,
         },
     });
     
     const data = await response.json();
-    setUsers(data);
-    setLoading(false);
+    
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    })
   };
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider value={{
-      users,
-      loading,
-      fetchUsers,
+      users: state.users,
+      loading: state.loading,
+      searchUsers,
     }}>
       {children}
     </GithubContext.Provider>
@@ -33,3 +51,10 @@ export const GithubProvider = ({ children }) => {
 };
 
 export default GithubContext;
+
+// REACT_APP_G
+// ITHUB_URL = "https://api.g
+// ithub.com"
+// REACT_APP_
+// GITHUB_TOKEN = "ghp_d4MDTH2pDDOrp
+// VI0AH1sfWyUuBQF2D0uKYyj"
