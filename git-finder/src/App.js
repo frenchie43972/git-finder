0d2077dd0ee742dc1da4772bbbe91895ac787fc5
French <1078924850117002@mil>
2023-02-21 15:00:37 +0900

    adding in user profile

--- git-finder/src/App.js
+++ git-finder/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import NotFound from "./pages/NotFound";
+import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from './context/github/GithubContext';
@@ -12,18 +14,20 @@ function App() {
     <GithubProvider>
       <AlertProvider>
         <Router>
-          <div className="flex flex-col justify-between h-screen">
-            <Navbar />
+            <div className="flex flex-col justify-between h-screen">
+              <Navbar />
             <main className="container mx-auto px-3 pb-12">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/notfound" element={<NotFound />} />
-                <Route path="/*" element={<NotFound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+              <Alert />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/user/:login" element={<User />} />
+                  <Route path="/notfound" element={<NotFound />} />
+                  <Route path="/*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
           </Router>
       </AlertProvider>
     </GithubProvider>
